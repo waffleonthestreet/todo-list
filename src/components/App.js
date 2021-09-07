d5249a5b85998def57d4faaf3dbca760c5c1ff0b
waffleonthestreet <woojeongkim610@gmail.com>
2021-09-07 13:40:25 +0900

    created PageTemplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import PageTemplate from "./PageTemplate";
 
 class App extends Component {
   render() {
-    return <div>일정관리</div>;
+    return <PageTemplate>안녕하세요</PageTemplate>;
   }
 }
 
